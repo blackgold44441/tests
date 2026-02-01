f08687c82b64d6ba07532034da816764fb24d317
Sergei Akimov <blackgold44441@gmail.com>
2026-02-01 17:47:44 +0300

    refactoring tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,4 @@
 from src.capitalize import capitalize
 
-if capitalize("hello") != "Hello":
-    raise Exception("Функция работает неверно!")
-
-if capitalize("") != "":
-    raise Exception("Функция работает неверно!")
-
-print("Все тесты пройдены!")
+assert capitalize("hello") == "Hello"
+assert capitalize("") == ""
